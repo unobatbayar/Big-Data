0c968d855a23d77612bc4bba91f0f686ad5368ec
unobatbayar <unobatbayar@yahoo.com>
2018-11-02 10:41:02 +0000

    added lambda use case

---
+++ lambda.py
@@ -0,0 +1,10 @@
+"""
+Big Data
+Learning to use Lambda for lambda architecture with Apache Spark
+
+@author unobatbayar
+"""
+
+x = lambda a: a * 10
+a = int(input('Input a number to multiplay by 10: '))
+print(x(5))
